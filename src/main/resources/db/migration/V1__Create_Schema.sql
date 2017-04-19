611f3c2b3acdf099f2a50378f4b4b0e506ddf6a2
Eluinhost <graham_howden1@yahoo.co.uk>
2017-04-20 00:29:21 +0100

    add users table to initial schema

--- src/main/resources/db/migration/V1__Create_Schema.sql
+++ src/main/resources/db/migration/V1__Create_Schema.sql
@@ -2,21 +2,9 @@
 CREATE SCHEMA IF NOT EXISTS "uuid";
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA uuid;
 
--- Holds short lived authorisation state info whilst sending off to 3rd party authorisation endpoints
--- CREATE TABLE IF NOT EXISTS "ExternalOauthState" (
---   id BIGSERIAL NOT NULL CONSTRAINT "PK_ExternalOauthState" PRIMARY KEY,
---   state UUID NOT NULL DEFAULT uuid.uuid_generate_v4(),
---   ip INET NOT NULL,
---   scopes TEXT[] NOT NULL CONSTRAINT "CK_ExternalOauthAuthorisations_scope" CHECK (array_length(scopes, 1) IS NOT NULL),
---   expires TIMESTAMPTZ NOT NULL
--- );
---
--- -- Holds actual oauth tokens after granted access for the user
--- CREATE TABLE IF NOT EXISTS "ExternalOauthAccessToken" (
---   id BIGSERIAL NOT NULL CONSTRAINT "PK_ExternalOauthAccessToken" PRIMARY KEY,
---   access_token TEXT NOT NULL,
---   token_type TEXT NOT NULL,
---   expires TIMESTAMPTZ NOT NULL,
---   scope TEXT[] NOT NULL,
---   refresh_token TEXT NOT NULL
--- );
+CREATE TABLE IF NOT EXISTS "Users" (
+  id BIGSERIAL NOT NULL CONSTRAINT "PK_Users" PRIMARY KEY,
+  username TEXT NOT NULL CONSTRAINT "UQ_Users_username" UNIQUE,
+  email TEXT NOT NULL,
+  created TIMESTAMPTZ NOT NULL
+);

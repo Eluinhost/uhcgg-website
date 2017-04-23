7264b593cb68c08954cabc17a04546e0f9f1e381
Eluinhost <graham_howden1@yahoo.co.uk>
2017-04-23 14:18:26 +0100

    screw the akka swagger lib, manual yaml bois

--- src/main/scala/routes/SwaggerDocumentationEndpoints.scala
+++ src/main/scala/routes/SwaggerDocumentationEndpoints.scala
@@ -1,29 +1,18 @@
 package routes
 
 import akka.actor.ActorSystem
-import akka.http.scaladsl.model.{HttpEntity, MediaTypes}
 import akka.http.scaladsl.server.Route
-import akka.stream.ActorMaterializer
 import akkahttptwirl.TwirlSupport
-import com.github.swagger.akka.{HasActorSystem, SwaggerHttpService}
 
-class SwaggerDocumentationEndpoints(implicit val actorSystem: ActorSystem)
-    extends SwaggerHttpService
-    with HasActorSystem
-    with HasRoutes
-    with TwirlSupport {
-  implicit val materializer = ActorMaterializer()
-  override val apiTypes     = Seq()
-  override val host         = "localhost:8000"
-  override val basePath     = "/"
-  override val apiDocsPath  = "docs"
+class SwaggerDocumentationEndpoints(implicit val actorSystem: ActorSystem) extends HasRoutes with TwirlSupport {
+  import akka.http.scaladsl.server.Directives._
 
-  override lazy val routes: Route =
-    path(apiDocsBase / "swagger.json") {
-      get {
-        complete(HttpEntity(MediaTypes.`application/json`, generateSwaggerJson))
+  val routes: Route =
+    (get & pathPrefix("docs")) {
+      pathEndOrSingleSlash {
+        complete(html.docs.render())
+      } ~ pathPrefix("yaml") {
+        getFromResourceDirectory("docs")
       }
-    } ~ (path(apiDocsBase) & pathEndOrSingleSlash) {
-      complete(html.docs.render())
     }
 }

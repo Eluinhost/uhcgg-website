c5ab644cb89c8b8f8aad2fcac0e9b3659784f04d
Eluinhost <graham_howden1@yahoo.co.uk>
2017-04-30 18:34:16 +0100

    add throttle to api calls

--- src/main/scala/reddit/ApiConsumer.scala
+++ src/main/scala/reddit/ApiConsumer.scala
@@ -4,19 +4,22 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.scaladsl.{Keep, Sink, Source}
-import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult}
+import akka.stream.{ActorMaterializer, OverflowStrategy, QueueOfferResult, ThrottleMode}
 
 import scala.concurrent.{ExecutionContext, Future, Promise}
 import scala.util.{Failure, Success}
 
 class ApiConsumer(host: String, queueSize: Int)(implicit actorSystem: ActorSystem) {
-  implicit val ec: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.duration._
+
+  implicit val ec: ExecutionContext            = scala.concurrent.ExecutionContext.Implicits.global
   implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   private[this] val pool = Http().cachedHostConnectionPoolHttps[Promise[HttpResponse]](host)
 
   private[this] val queue = Source
     .queue[(HttpRequest, Promise[HttpResponse])](queueSize, OverflowStrategy.dropNew)
+    .throttle(30, 1 minute, 1, ThrottleMode.Shaping)
     .via(pool)
     .toMat(Sink.foreach({
       case ((Success(response), promise))  ⇒ promise.success(response)

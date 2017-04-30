602fc1bdcdaaa9214a92920afc53d437bb439fca
Eluinhost <graham_howden1@yahoo.co.uk>
2017-04-30 10:40:01 +0100

    pure function register form

--- src/main/javascript/app/RegisterForm.js
+++ src/main/javascript/app/RegisterForm.js
@@ -1,86 +1,86 @@
 import { Form, Icon, Input, Button } from 'antd';
 const FormItem = Form.Item;
+import { pure } from 'recompose';
 
-class RegisterFormComponent extends React.Component {
-    formItemLayout = {
-        labelCol: {
-            xs: { span: 24 },
-            sm: { span: 6 },
-        },
-        wrapperCol: {
-            xs: { span: 24 },
-            sm: { span: 14 },
-        },
-    };
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 14 },
+    },
+};
 
-    emailRules = [
-        { required: true, message: 'An email must be provided' },
-        { pattern: /^^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/, message: 'Invalid email provided' }
-    ];
+const emailRules = [
+    { required: true, message: 'An email must be provided' },
+    { pattern: /^^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/, message: 'Invalid email provided' }
+];
 
-    passwordRules = [
-        { required: true, message: 'Please input your chosen password' },
-        { pattern: /[a-z]+/, message: 'Must contain at least 1 lower case character'},
-        { pattern: /[A-Z]+/, message: 'Must contain at least 1 upper case character'},
-        { pattern: /[0-9]+/, message: 'Must contain at least 1 digit'},
-        { pattern: /[^a-zA-Z0-9]+/, message: 'Must contain at least 1 special character'},
-        { min: 8, message: 'Must contain at least 8 characters'}
-    ];
+const passwordRules = [
+    { required: true, message: 'Please input your chosen password' },
+    { pattern: /[a-z]+/, message: 'Must contain at least 1 lower case character'},
+    { pattern: /[A-Z]+/, message: 'Must contain at least 1 upper case character'},
+    { pattern: /[0-9]+/, message: 'Must contain at least 1 digit'},
+    { pattern: /[^a-zA-Z0-9]+/, message: 'Must contain at least 1 special character'},
+    { min: 8, message: 'Must contain at least 8 characters'}
+];
 
-    repeatPasswordRules = [
-        { required: true, validator: (rule, value, callback) => {
-            if (value !== this.props.form.getFieldValue('password')) {
-                callback('Password does not match');
-            } else {
-                callback();
-            }
-        }}
-    ];
+const repeatPasswordRules = form => [
+    { required: true, validator: (rule, value, callback) => {
+        if (value !== form.getFieldValue('password')) {
+            callback('Passwords do not match');
+        } else {
+            callback();
+        }
+    }}
+];
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFieldsAndScroll((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
+const handleSubmit = form => e => {
+    e.preventDefault();
 
-                fetch('/api/register', {
-                    method: 'POST',
-                    body: JSON.stringify(values),
-                    credentials: 'same-origin',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                })
+    form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+            console.log('Received values of form: ', values);
 
-                // TODO handle response
-            }
-        });
-    };
+            fetch('/api/register', {
+                method: 'POST',
+                body: JSON.stringify(values),
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
 
-    render = () => <Form className="login-form" onSubmit={this.handleSubmit}>
-        <FormItem { ...this.formItemLayout} label="Username">
-            <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} value={this.props.username} disabled={true} />
-        </FormItem>
-        <FormItem {...this.formItemLayout} label="Email">
-            {this.props.form.getFieldDecorator('email', {rules: this.emailRules})(
-                <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="Email"/>
-            )}
-        </FormItem>
-        <FormItem {...this.formItemLayout} label="Password">
-            {this.props.form.getFieldDecorator('password', {rules: this.passwordRules})(
-                <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password" placeholder="Password"/>
-            )}
-        </FormItem>
-        <FormItem {...this.formItemLayout} label="Repeat Password">
-            {this.props.form.getFieldDecorator('confirm', {rules: this.repeatPasswordRules})(
-                <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password" placeholder="Repeat Password"/>
-            )}
-        </FormItem>
-        <FormItem {...this.formItemLayout}>
-            <Button type="primary" htmlType="submit" size="large">Register</Button>
-        </FormItem>
-    </Form>
-}
+            // TODO handle response
+        }
+    });
+};
+
+const RegisterFormComponent = pure(({ form, username }) =>  <Form className="login-form" onSubmit={handleSubmit(form)}>
+    <FormItem { ...formItemLayout} label="Username">
+        <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} value={username} disabled={true} />
+    </FormItem>
+    <FormItem {...formItemLayout} label="Email">
+        {form.getFieldDecorator('email', {rules: emailRules})(
+            <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="Email"/>
+        )}
+    </FormItem>
+    <FormItem {...formItemLayout} label="Password">
+        {form.getFieldDecorator('password', {rules: passwordRules})(
+            <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password" placeholder="Password"/>
+        )}
+    </FormItem>
+    <FormItem {...formItemLayout} label="Confirm Password">
+        {form.getFieldDecorator('confirm', {rules: repeatPasswordRules(form)})(
+            <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password" placeholder="Confirm Password"/>
+        )}
+    </FormItem>
+    <FormItem {...formItemLayout}>
+        <Button type="primary" htmlType="submit" size="large">Register</Button>
+    </FormItem>
+</Form>);
 
 export const RegisterForm = Form.create()(RegisterFormComponent);
 

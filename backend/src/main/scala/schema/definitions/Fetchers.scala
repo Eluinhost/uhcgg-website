9791b0e703b5ae4a21944ef767f18beaa201c47a
Eluinhost <graham_howden1@yahoo.co.uk>
2017-05-06 18:55:36 +0100

    simplify user queries

--- backend/src/main/scala/schema/definitions/Fetchers.scala
+++ backend/src/main/scala/schema/definitions/Fetchers.scala
@@ -2,17 +2,17 @@ package schema.definitions
 
 import java.util.UUID
 
-import sangria.execution.deferred.{Fetcher, HasId, RelationIds}
+import sangria.execution.deferred._
 import schema.SchemaContext
 import schema.model.Ban
 
 object Fetchers {
   val users = Fetcher.caching(
-    (ctx: SchemaContext, ids: Seq[UUID]) ⇒ ctx.users.getByIds(ids)
+    fetch = (ctx: SchemaContext, ids: Seq[UUID]) ⇒ ctx.users.getByIds(ids)
   )(HasId(_.id))
 
   val bans = Fetcher.relCaching(
-    (ctx: SchemaContext, ids: Seq[Long]) ⇒ ctx.bans.getBansByIds(ids),
-    (ctx: SchemaContext, ids: RelationIds[Ban]) ⇒ ctx.bans.getByRelations(ids)
+    fetch = (ctx: SchemaContext, ids: Seq[Long]) ⇒ ctx.bans.getBansByIds(ids),
+    fetchRel = (ctx: SchemaContext, ids: RelationIds[Ban]) ⇒ ctx.bans.getByRelations(ids)
   )(HasId(_.id))
 }
--- backend/src/main/scala/schema/definitions/UsersQueries.scala
+++ backend/src/main/scala/schema/definitions/UsersQueries.scala
@@ -4,48 +4,45 @@ import sangria.schema._
 import schema.SchemaContext
 import schema.scalars.CustomScalars._
 
-import scala.concurrent.Future
-
 class UsersQueries {
-  val idArg       = Argument("id", OptionInputType(UuidType), description = "ID to match")
-  val usernameArg = Argument("username", OptionInputType(StringType), description = "Usernames to match")
+  val idArg       = Argument(name = "id", argumentType = UuidType, description = "ID to match")
+  val usernameArg = Argument(name = "username", argumentType = StringType, description = "Username to match")
 
-  val idsArg = Argument("ids", OptionInputType(ListInputType(UuidType)), description = "List of IDs to match")
-  val usernamesArg =
-    Argument("usernames", OptionInputType(ListInputType(StringType)), description = "List of usernames to match")
+  val idsArg = Argument(name = "ids", argumentType = ListInputType(UuidType), description = "List of IDs to match")
+  val usernamesArg = Argument(
+    name = "usernames",
+    argumentType = ListInputType(StringType),
+    description = "List of usernames to match"
+  )
 
   val query: List[Field[SchemaContext, Unit]] = fields(
     Field(
-      "user",
-      OptionType(Types.UserType),
-      arguments = idArg :: usernameArg :: Nil,
-      resolve = ctx ⇒
-        ctx.withArgs(idArg, usernameArg) {
-          case (Some(_), Some(_)) | (None, None) ⇒
-            Future failed new IllegalArgumentException("Must provide either an ID or a username")
-          case (Some(id), _)       ⇒ ctx.ctx.users.getById(id)
-          case (_, Some(username)) ⇒ ctx.ctx.users.getByUsername(username)
-      },
-      description =
-        Some("Look up a single user either by their ID or their username. Arguments are mutually exclusive")
+      name = "userById",
+      fieldType = OptionType(Types.UserType),
+      arguments = idArg :: Nil,
+      resolve = ctx ⇒ Fetchers.users.defer(ctx arg idArg),
+      description = Some("Fetch a user with the given ID")
     ),
     Field(
-      "users",
-      ListType(Types.UserType),
-      arguments = idsArg :: usernamesArg :: Nil,
-      resolve = ctx ⇒
-        ctx.withArgs(idsArg, usernamesArg) { (maybeIds, maybeUsernames) ⇒
-          import scala.concurrent.ExecutionContext.Implicits.global
-
-          val idsFuture       = maybeIds.map(ctx.ctx.users.getByIds).getOrElse(Future successful List())
-          val usernamesFuture = maybeUsernames.map(ctx.ctx.users.getByUsernames).getOrElse(Future successful List())
-
-          for {
-            byId       ← idsFuture
-            byUsername ← usernamesFuture
-          } yield byId ::: byUsername
-      },
-      description = Some("Look up users based on ids and/or usernames")
+      name = "userByUsername",
+      fieldType = OptionType(Types.UserType),
+      arguments = usernameArg :: Nil,
+      resolve = ctx ⇒ ctx.ctx.users.getByUsername(ctx arg usernameArg),
+      description = Some("Fetch a user with the given username")
+    ),
+    Field(
+      name = "usersByIds",
+      fieldType = ListType(Types.UserType),
+      arguments = idsArg :: Nil,
+      resolve = ctx ⇒ Fetchers.users.deferSeq(ctx arg idsArg),
+      description = Some("Fetch users with the given IDs")
+    ),
+    Field(
+      name = "usersByUsernames",
+      fieldType = ListType(Types.UserType),
+      arguments = usernamesArg :: Nil,
+      resolve = ctx ⇒ ctx.ctx.users.getByUsernames(ctx arg usernamesArg),
+      description = Some("Fetch users with the given usernames")
     )
   )
 }

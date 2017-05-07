8c1b9eb2eb31b67a5d759208744f2ee690baa79c
Eluinhost <graham_howden1@yahoo.co.uk>
2017-05-07 09:40:14 +0100

    add docs for user role

--- backend/src/main/scala/schema/model/UserRole.scala
+++ backend/src/main/scala/schema/model/UserRole.scala
@@ -2,4 +2,8 @@ package schema.model
 
 import java.util.UUID
 
+import sangria.macros.derive.{GraphQLDescription, GraphQLName}
+
+@GraphQLName("UserRole")
+@GraphQLDescription("Maps users to roles")
 case class UserRole(userId: UUID, roleId: Int)

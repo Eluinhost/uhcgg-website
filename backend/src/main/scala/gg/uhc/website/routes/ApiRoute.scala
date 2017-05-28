40504affa698e53e9acc3cf5252ceadfb855c847
Eluinhost <graham_howden1@yahoo.co.uk>
2017-05-29 00:40:52 +0100

    add cors headers to api for now

--- backend/src/main/scala/gg/uhc/website/routes/ApiRoute.scala
+++ backend/src/main/scala/gg/uhc/website/routes/ApiRoute.scala
@@ -2,11 +2,13 @@ package gg.uhc.website.routes
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
+import ch.megard.akka.http.cors.CorsDirectives
 
 class ApiRoute(registerRoute: RegisterRoute, docsRoute: DocumentationRoute, graphql: GraphqlRoute)
-    extends PartialRoute {
+    extends PartialRoute
+    with CorsDirectives {
 
-  override val route: Route = pathPrefix("api") {
+  override val route: Route = (cors() & pathPrefix ("api")) {
     registerRoute.route ~ graphql.route ~ docsRoute.route ~ complete(StatusCodes.NotFound) // Return 404 for anything else starting /api
   }
 }

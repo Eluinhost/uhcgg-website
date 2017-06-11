6cdc3c110429b2bbf4c485b72637b9502f083dbd
Eluinhost <graham_howden1@yahoo.co.uk>
2017-06-11 13:30:58 +0100

    fetch more data for networks page

--- frontend/src/main/typescript/graphql.tsx
+++ frontend/src/main/typescript/graphql.tsx
@@ -1,21 +1,37 @@
-//  This file was automatically generated and should not be edited.
 /* tslint:disable */
+//  This file was automatically generated and should not be edited.
 
 export interface NetworksQuery {
   // Fetches all versions
   networks: Array< {
+    __typename: string,
     // The unique ID of this item
     id: string,
     // The unique name of this network
     name: string,
+    // The unique tag of this network
+    tag: string,
+    // The owner of the network, has full control
+    owner: {
+      __typename: string,
+      // The unique username of this user
+      username: string,
+    },
     // All of the servers belonging to this network
     servers: Array< {
+      __typename: string,
       // The unique ID of this item
       id: string,
       // The unique name (per-network) of this server
       name: string,
       // The direct connect IP address of the server
       ip: string,
+      // Optional address to connect to the server
+      address: string | null,
+      // Text description of where about the server is hosted
+      location: string,
+      // Optional port to use to connect
+      port: number | null,
     } >,
   } >;
 }
@@ -28,6 +44,7 @@ export interface RegisterMutationVariables {
 
 export interface RegisterMutation {
   register: {
+    __typename: string,
     // The unique ID of this item
     id: string,
     // The unique username of this user

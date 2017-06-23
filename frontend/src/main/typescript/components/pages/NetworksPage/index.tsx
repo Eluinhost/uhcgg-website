b159f914cb8dbda409da9f1c9abec6dea98f6ad4
Eluinhost <graham_howden1@yahoo.co.uk>
2017-06-23 09:40:11 +0100

    break away from the edges

--- frontend/src/main/typescript/components/pages/NetworksPage/index.tsx
+++ frontend/src/main/typescript/components/pages/NetworksPage/index.tsx
@@ -2,4 +2,7 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { NetworkList } from './NetworkList';
 
-export const NetworksPage: React.SFC<RouteComponentProps<void>> = () => <NetworkList />;
+export const NetworksPage: React.SFC<RouteComponentProps<void>> = () =>
+    <div style={{ margin: 20 }}>
+        <NetworkList />
+    </div>;
